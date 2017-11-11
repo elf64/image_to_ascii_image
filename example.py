6b03aebeb866adbaee18316056c92524712d8313
PukaPy <pukalordu@yahoo.com>
2017-11-11 21:26:26 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,10 @@
 import pyascii
 
-my_img = pyascii.Pyascii("Path-to-image", "Path-to-save-ascii-image")
+my_img = pyascii.Pyascii(
+        "/home/puka/Desktop/Mona_Lisa.jpg",
+        "/home/puka/Desktop/test6.jpg",
+        enh_color=2.0
+        )
 my_img.default_convert()
 my_img.image_show()
 my_img.image_save()
